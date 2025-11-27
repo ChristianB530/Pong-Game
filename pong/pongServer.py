b77eea240f847c95ffc1197c6c663699d2fa2b45
Ben Carey <benjamin.james.carey3@gmail.com>
2025-11-26 22:55:28 -0500

    misc: type linting

--- pong/pongServer.py
+++ pong/pongServer.py
@@ -9,6 +9,7 @@
 import socket
 import threading
 from time import sleep
+from typing import Tuple
 
 # =====================================================
 # Author: Ben Carey, Caleb Burnett
@@ -20,7 +21,7 @@ from time import sleep
 # - side_str never changes.
 # =====================================================
 class Connection:
-    def __init__(self, conn, side_str) -> None:
+    def __init__(self, conn: socket.SocketType, side_str: str) -> None:
         self.conn: socket.SocketType = conn
         self.sync: int = 0
         self.side_str: str = side_str
@@ -35,14 +36,14 @@ class Connection:
 # - Used to update game state sent to a spectator.
 # =====================================================
 class Spectator:
-    def __init__(self, conn):
-        self.conn = conn
-        self.left_x = 0
-        self.left_y = 0
-        self.left_score = 0
-        self.right_x = 0
-        self.right_y = 0
-        self.right_score = 0
+    def __init__(self, conn: socket.SocketType) -> None:
+        self.conn: socket.SocketType = conn
+        self.left_x: int = 0
+        self.left_y: int = 0
+        self.left_score: int = 0
+        self.right_x: int = 0
+        self.right_y: int = 0
+        self.right_score: int = 0
 
 # =====================================================
 # Author: Ben Carey, Caleb Burnett, Christian Brewer
@@ -54,24 +55,24 @@ class Spectator:
 #   client) to the other clients.
 # =====================================================
 class Server:
-    def __init__(self, port: int):
+    def __init__(self, port: int) -> None:
         #Window Size
-        self.window_width = 800
-        self.window_height = 600
+        self.window_width: int = 800
+        self.window_height: int = 600
         #Server Socket
-        self.port = port
-        self.leftPaddleState = ""
-        self.rightPaddleState = ""
-        self.sock = None
-        self.ball_pos = [0, 0]
+        self.port: int = port
+        self.leftPaddleState: str = ""
+        self.rightPaddleState: str = ""
+        self.sock: socket.SocketType | None = None
+        self.ball_pos: list[int] = [0, 0]
 
-        self.left_connection = None
-        self.left_flagged = False
-        self.right_connection = None
-        self.right_flagged = False
+        self.left_connection: Connection | None = None
+        self.left_flagged: bool = False
+        self.right_connection: Connection | None = None
+        self.right_flagged: bool = False
 
         #Spectators list
-        self.spectators: list[Connection] = []
+        self.spectators: list[Spectator] = []
         # x,y coords for each
         # may be useful to pull out paddle states into classes, but might be too much
 
@@ -82,7 +83,7 @@ class Server:
 # - Begins running the server. Should only be called once, and will block until
 #   the server closes.
 # =====================================================
-    def start(self):
+    def start(self) -> None:
         # Create a new socket.
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # Set configuration so that we free the port once the server closes.
@@ -108,7 +109,7 @@ class Server:
 # Email Address: bjca251@uky.edu, cdbu246@uky.edu, cebr276@uky.edu
 # Uses and Invariants: - Called in a newly spawned thread for each client that connects to the server. - Processes all packets sent by the client until the client disconnects.
 # =====================================================
-    def handleClient(self, conn: socket.SocketType, addr: str):
+    def handleClient(self, conn: socket.SocketType, addr: str) -> None:
         print("connection get!")
         side = ""
         recv = None
@@ -122,10 +123,10 @@ class Server:
             if args[0] == "get_rekt":
                 if self.left_connection is None:
                     side_str = "left"
-                    self.left_connection = Connection(conn, 0)
+                    self.left_connection = Connection(conn, side_str)
                 elif self.right_connection is None:
                     side_str = "right"
-                    self.right_connection = Connection(conn, 0)
+                    self.right_connection = Connection(conn, side_str)
                 else:
                     side_str = "spectator"
                     self.spectators.append(Spectator(conn))
@@ -140,8 +141,8 @@ class Server:
                 if args[2] == "left":
                     self.left_connection.sync = sync
                     self.left_flagged = False
-                    self.ball_pos[0] = args[3]
-                    self.ball_pos[1] = args[4]
+                    self.ball_pos[0] = int(args[3])
+                    self.ball_pos[1] = int(args[4])
                 else:
                     self.right_connection.sync = sync
                     self.right_flagged = False
@@ -160,9 +161,9 @@ class Server:
                     self.right_connection.moving = args[4]
 
                 if self.left_connection:
-                    self.left_connection.score = args[5]
+                    self.left_connection.score = int(args[5])
                 if self.right_connection:
-                    self.right_connection.score = args[6]
+                    self.right_connection.score = int(args[6])
             # Handle if client requests a restart.
             elif args[0] == "restart":
                 #Reset the position and score
@@ -177,66 +178,6 @@ class Server:
                 # Notify both players so they reset locally too
                 self.left_connection.conn.send(b"restart 0 0 0 0 0 0 0 \n")
                 self.right_connection.conn.send(b"restart 0 0 0 0 0 0 0 \n")
-
-            print(f"Received from {addr}: {recv}")
-
-            args = recv.split(' ')
-            #Player try to join
-            if args[0] == "get_rekt":
-                if self.left_connection is None:
-                    side_str = "left"
-                    self.left_connection = Connection(conn, 0)
-                    #self.left_connection.x = 10
-                    #self.left_connection.y = (self.screenHeight/2) - (25)
-                elif self.right_connection is None:
-                    side_str = "right"
-                    self.right_connection = Connection(conn, 0)
-                    #self.right_connection.x = self.screenWidth - 20
-                    #self.right_connection.y = (self.screenHeight/2) - (25)
-                else:
-                    side_str = "spectator"
-                    self.spectators.append(Spectator(conn))
-
-                response = "no_u 800 600 " + side_str
-                conn.send(bytes(response, "utf-8"))
-                side = side_str
-            #Sync logic for client
-            elif args[0] == "sync":
-                sync = int(args[1])
-                if args[2] == "left":
-                    self.left_connection.sync = sync
-                    self.left_flagged = False
-                else:
-                    self.right_connection.sync = sync
-                    self.right_flagged = False
-            #Paddle info update
-            #args = ["paddle",side, x, y, moving (direction), lscore, rscore]
-            elif args[0] == "paddle":
-                side = args[1]
-                if side == "left":
-                    self.left_connection.x = int(args[2])
-                    self.left_connection.y = int(args[3])
-                    self.left_connection.moving = args[4]
-                    self.left_connection.score = args[5]
-                elif side == "right":
-                    self.right_connection.x = int(args[2])
-                    self.right_connection.y = int(args[3])
-                    self.right_connection.moving = args[4]
-                    self.right_connection.score = args[6]
-            elif args[0] == "restar":
-                #Reset the position and score
-                #This will reset if one party decide to do the reset.
-                self.left_connection.x = 50
-                self.left_connection.y = self.window_height // 2
-                self.left_connection.score = 0
-                self.right_connection.x = self.window_width - 50
-                self.right_connection.y = self.window_height // 2
-                self.right_connection.score = 0
-
-                # Notify both players so they reset locally too
-                self.left_connection.conn.send(b"reset_ack")
-                self.right_connection.conn.send(b"reset_ack")
-
 
 
         #Handle disconnection
@@ -265,7 +206,7 @@ class Server:
 #   parameters to member functions properly when calling the Thread
 #   constructor.
 # =====================================================
-    def update(self, foo: int):
+    def update(self, foo: int) -> None:
         while True:
             sleep(0.01)
 
@@ -287,7 +228,7 @@ class Server:
                 self.greenFlag(self.left_connection, self.right_connection)
                 self.right_flagged = True
 
-    def greenFlag(self, to_update: Connection, opponent: Connection, send_ball = False):
+    def greenFlag(self, to_update: Connection, opponent: Connection, send_ball = False) -> None:
         message = "sync " + str(opponent.sync) + " " + str(opponent.x) + " " + str(opponent.y) + " " + str(self.left_connection.score) + " " + str(self.right_connection.score)
         if send_ball:
             message += " " + str(self.ball_pos[0]) + " " + str(self.ball_pos[1])
